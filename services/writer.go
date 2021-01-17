4b810fec71bc3e0c0c331b80d3d6c3d68d9a7ebb
Max Kuderko <kuderko@gmail.com>
2021-01-18 01:08:57 +0200

    + use only one instance of s3 connection (build the pool in the writer if needed) + better utilize the writerCloser interface

--- services/writer.go
+++ services/writer.go
@@ -5,43 +5,22 @@ import (
 	jsoniter "github.com/json-iterator/go"
 	"github.com/maxim-kuderko/plutos/entities"
 	"io"
-	"math/rand"
-	"runtime"
 	"sync"
 )
 
 type Writer struct {
-	bufferPools []*sync.Pool
+	bufferPools *sync.Pool
 	driver      io.WriteCloser
 }
 
 func NewWriter(d io.WriteCloser) *Writer {
-	poolSize := runtime.NumCPU()
-	pools := make([]*sync.Pool, 0, poolSize)
-	for i := 0; i <= poolSize; i++ {
-		pools = append(pools, &sync.Pool{New: func() interface{} {
-			return bytes.NewBuffer(nil)
-		}})
-	}
-	return &Writer{driver: d, bufferPools: pools}
+	return &Writer{driver: d, bufferPools: &sync.Pool{New: func() interface{} {
+		return bytes.NewBuffer(nil)
+	}}}
 }
 
-var SEPERATOR = byte('\n')
-
 func (w *Writer) Write(e entities.Event) error {
-	r := rand.Int() % len(w.bufferPools)
-	buff := w.bufferPools[r].Get().(*bytes.Buffer)
-	defer func() {
-		buff.Reset()
-		w.bufferPools[r].Put(buff)
-	}()
-	err := jsoniter.ConfigFastest.NewEncoder(buff).Encode(e)
-	if err != nil {
-		return err
-	}
-	buff.WriteByte(SEPERATOR)
-	_, err = w.driver.Write(buff.Bytes())
-	return err
+	return jsoniter.ConfigFastest.NewEncoder(w.driver).Encode(e)
 
 }
 
