1bca80d442d071ac6de00701b0673bd1573c105d
Max Kuderko <kuderko@gmail.com>
2021-02-11 10:37:03 +0200

    + null handler

--- cmd/fasthttp.go
+++ cmd/fasthttp.go
@@ -18,7 +18,6 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
-	"time"
 )
 
 func main() {
@@ -95,7 +94,7 @@ func getEnrichment(ctx *routing.Context) plutos.Enrichment {
 }
 
 func generateMetadata() plutos.Metadata {
-	return plutos.Metadata{WrittenAt: time.Now().Format(time.RFC3339), RequestID: uuid.New().String()}
+	return plutos.Metadata{ /*WrittenAt: time.Now().Format(time.RFC3339),*/ RequestID: uuid.New().String()}
 }
 
 func EventFromRoutingCtxPOST(ctx *routing.Context) (plutos.Event, error) {
