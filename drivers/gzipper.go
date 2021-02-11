1da7f3106ec2b7b755b38f17692c33b8d1d6ca6f
Max Kuderko <kuderko@gmail.com>
2021-02-11 19:19:05 +0200

    + add pseudorandom request_id, with combination of the uuid of the file in s3 it be unique enough

--- drivers/gzipper.go
+++ drivers/gzipper.go
@@ -1,7 +1,6 @@
 package drivers
 
 import (
-	"bufio"
 	"github.com/golang/snappy"
 	"io"
 	"os"
@@ -14,17 +13,14 @@ var (
 
 type Compressor struct {
 	origWriter Driver
-	buff       *bufio.Writer
 	w          io.WriteCloser
 }
 
 func NewCompressor(w func() Driver) (Driver, error) {
 	orig := w()
-	buff := bufio.NewWriterSize(orig, 1<<20)
 	gw := snappy.NewBufferedWriter(orig)
 	return &Compressor{
 		origWriter: orig,
-		buff:       buff,
 		w:          gw,
 	}, nil
 }
@@ -35,9 +31,6 @@ func (g *Compressor) Write(b []byte) (int, error) {
 
 func (g *Compressor) Close() error {
 	if err := g.w.Close(); err != nil {
-		return err
-	}
-	if err := g.buff.Flush(); err != nil {
 		return err
 	}
 
